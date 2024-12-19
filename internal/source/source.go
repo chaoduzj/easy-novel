c100085e208a6d3c5c5a9431fa979eaaf85045da
Fang Yuan <wojiushifangyuanlove@gmail.com>
2024-12-19 09:47:56 +0800

    feat: embed add rule
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- internal/source/source.go
+++ internal/source/source.go
@@ -1,16 +1,18 @@
 package source
 
 import (
+	"embed"
 	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
 	"sync"
 
 	"github.com/767829413/easy-novel/internal/model"
 	"github.com/767829413/easy-novel/pkg/utils"
 	"github.com/fatih/color"
 )
+
+//go:embed rule/*.json
+var ruleFS embed.FS
 
 var ruleCache sync.Map
 
@@ -20,14 +22,14 @@ func GetRuleBySourceID(sourceId int) *model.Rule {
 	}
 
 	// 构建文件路径
-	filePath := filepath.Join("rule", fmt.Sprintf("rule%d.json", sourceId))
+	filePath := fmt.Sprintf("rule/rule%d.json", sourceId)
 	// 读取文件内容
-	ruleData, err := os.ReadFile(filePath)
+	ruleData, err := ruleFS.ReadFile(filePath)
 	if err != nil {
 		red := utils.GetColorIns(color.FgRed).SprintFunc()
 		fmt.Println(red("书源规则初始化失败，请检查配置项 source-id"))
 		fmt.Println(red("错误信息："), err.Error())
-		os.Exit(1)
+		return nil
 	}
 
 	// 解析 JSON 到 Rule 结构体
@@ -36,7 +38,7 @@ func GetRuleBySourceID(sourceId int) *model.Rule {
 	if err != nil {
 		red := utils.GetColorIns(color.FgRed).SprintFunc()
 		fmt.Println(red("内部解析错误："), err.Error())
-		os.Exit(1)
+		return nil
 	}
 
 	// 使用 LoadOrStore 方法来确保线程安全的写入
