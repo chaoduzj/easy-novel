434ab1fb16a3f069b0e375a4764194d54f8702d5
Fang Yuan <wojiushifangyuanlove@gmail.com>
2024-12-19 10:05:30 +0800

    feat: update action
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- internal/tools/merge/merge_handler.go
+++ internal/tools/merge/merge_handler.go
@@ -11,23 +11,18 @@ import (
 
 func MergeSaveHandler(book *model.Book, dirPath string) error {
 	conf := config.GetConf()
-	s := fmt.Sprintf("\n<== 《%s》（%s）下载完毕，", book.BookName, book.Author)
-
-	switch conf.Base.Extname {
-	case definition.NovelExtname_TXT, definition.NovelExtname_EPUB:
-		s += fmt.Sprintf("正在合并为 %s", strings.ToUpper(conf.Base.Extname))
-	case definition.NovelExtname_HTML:
-		s += "正在生成 HTML 目录文件"
-	}
-	fmt.Println(s + " ...")
-
+	s := fmt.Sprintf(
+		"\n<== 《%s》（%s）下载完毕，正在合并为 %s ...\n",
+		book.BookName,
+		book.Author,
+		strings.ToUpper(conf.Base.Extname),
+	)
+	fmt.Println(s)
 	switch conf.Base.Extname {
 	case definition.NovelExtname_TXT:
 		return txtMergeHandler(book, dirPath)
 	case definition.NovelExtname_EPUB:
 		return epubMergeHandler(book, dirPath)
-	case definition.NovelExtname_HTML:
-		return htmlMergeHandler(book, dirPath)
 	default:
 		return fmt.Errorf("unsupported extension: %s", conf.Base.Extname)
 	}
